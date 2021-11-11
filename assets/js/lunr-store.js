b555fc264620a03bff38d112e82da20f1259e103
shawngraham <shawngraham@users.noreply.github.com>
2021-11-11 19:33:55 +0000

    deploy: 8316b2c797fbd9e23d0572a7c62fd1a09ccde13b

---
+++ assets/js/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{  
+"title": "Good News – Power (radio episode)","date": "1905-04-30","creator": "Robinson, Frank B.","description": "Example locally hosted audio item. Psychiana radio program episode begins with assurance that there is a Power that can get people what they desire. Robinson then discusses his early life failures and childhood seeking of some force that could turn his failures into success. He asserts the God-Power can turn our failures into success and bring us material and spiritual abundance.","subject": "Frank B. Robinson; Failure; Success; Material Abundance","location": "Moscow, Idaho", 
+"id": "example_003"
+
+},{  
+"title": "University of Idaho vs. University of Southern California (Football), 10/30/1925","date": "1925-10-30","creator": "Vandal Athletics","description": "Example YouTube video item. Idaho Football vs. University of Southern California 10/30/1925 at Neale Stadium in Moscow, ID. Score: 7 - 51 (L).","subject": "American Football","location": "Moscow, Idaho", 
+"id": "example_004"
+
+},{  
+"title": "Interview with K. Silem Mohammad","date": "2014-06-16","creator": "Becker, Devin","description": "Example Vimeo video item. Oral history interview with poet K. Silem Mohammad discussing recent changes in writing practices that occurred due to the advent of the computer and the arrival of the digital age.","subject": "Poetry","location": "Ashland, OR", 
+"id": "example_005"
+
+},{  
+"title": "Ford pumper used for slash burning control","date": "1932-09-01","creator": "Thompson, J. B.","description": "Example image item externally hosted on CONTENTdm. Ford pumper used for slash burning control. September 1932. From the photo series depicting the broadcast burn at the mouth of Benton Creek.","subject": "forestry","location": "Bonner County;Priest River Experimental Forest;Benton Creek", 
+"id": "example_006"
+
+},{  
+"title": "Influence of Fishway Placement on Fallback of Adult Salmon at the Bonneville Dam on the Columbia River","date": "1905-06-25","creator": "Reischel, T.S.; Bjornn, T.C.","description": "Example metadata-only record with link to external source. Journal article.","subject": "fisheries management","location": "Bonneville Dam", 
+"id": "example_007"
+
+},{  
+"title": "I.3.30 Pompeii. Small house. Excavated 1872.","date": "2002","creator": "Pompeii in Pictures","description": "I.3.30 Pompeii. Entrance fauces or corridor looking north, across the west portico of the peristyle garden.","subject": "Pompeii location of skeletons","location": "Pompeii", 
+"id": "example_008"
+
+}];
